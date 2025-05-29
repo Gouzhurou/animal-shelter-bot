0cd874194d80ae47a239efb4b88d61c512fd7274
gouzhurou <arlantamom1999@gmail.com>
2025-05-30 00:19:55 +0300

    feat: edit init.sql

--- sql_scripts/init.sql
+++ sql_scripts/init.sql
@@ -1,7 +1,8 @@
 CREATE TABLE client (
-    id      BIGSERIAL PRIMARY KEY,
+    user_id VARCHAR(30) PRIMARY KEY,
     name    VARCHAR(30) NOT NULL,
     surname VARCHAR(30) NOT NULL,
+    number  VARCHAR(20) NOT NULL,
     email   VARCHAR(50) NOT NULL,
     age     INTEGER     NOT NULL CHECK (age >= 0),
     city    VARCHAR(30) NOT NULL,
